7047d3a0e69783af300cc0ded7b80ec072e54126
Mirko <mirkocaccavone03@gmail.com>
2025-02-26 14:41:37 +0100

    [add] ChiSiamo, HomePage, and PostListPage components

---
+++ src/pages/ChiSiamo.jsx
@@ -0,0 +1,9 @@
+export default function ChiSiamo() {
+    return (
+        <div>
+            <h1>
+                CI TROVIAMO SULLA PAGINA CHI SIAMO
+            </h1>
+        </div>
+    )
+}
---
+++ src/pages/PostListPage.jsx
@@ -0,0 +1,15 @@
+// import di PostList.jsx per mostrarlo nella pagina corrente
+import PostList from "../components/PostList"
+
+export default function PostListPage() {
+    return (
+        <>
+            <div>
+                <h1>
+                    PAGINA DEI POST
+                </h1>
+            </div>
+            <PostList />
+        </>
+    )
+}
