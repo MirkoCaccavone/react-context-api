6675aa0e1d2879337cbbbcdea571c1f3139c609a
Mirko <mirkocaccavone03@gmail.com>
2025-02-26 17:15:31 +0100

    [feat] integrate GlobalContext for post management and fetch posts on load

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Layout
 import DefaultLayout from "./layouts/DefaultLayout";
 
+
+// importiamo il contesto creato (Global)
+import GlobalContext from "./context/GlobalContext"
+
+
+// gestione dati post  per listato
+import { useState, useEffect } from 'react';
+import axios from "axios";
+
 // Pages
 import HomePage from "./pages/HomePage"
 import ChiSiamo from "./pages/ChiSiamo"
@@ -17,26 +26,42 @@ import PostCreatePage from "./pages/PostCreatePage"
 
 function App() {
 
+  // utilizzo dello useState per la gestione dei data (array di oggetti post)
+  const [posts, setPosts] = useState([]);
+
+  // funzione di gestione chiamata all'API
+  function fetchPosts() {
+    axios.get("http://localhost:3000/posts")
+      .then(res => setPosts(res.data))
+      .catch(error => console.log(error))
+  }
+
+  // richiamo la funzione di richiesta dati al caricamento del componente
+  // Solo al primo rendering
+  useEffect(fetchPosts, []);
+
 
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* DefaultLayout viene utilizzato come layout di default per le rotte */}
-        <Route element={<DefaultLayout />}>
-          {/* Definizione delle rotte */}
-          <Route path="/" element={<HomePage />} />
-          <Route path="/chisiamo" element={<ChiSiamo />} />
-          <Route path="/posts">
-            <Route index element={<PostListPage />} />
-            <Route path="create" element={<PostCreatePage />} />
-            <Route path=":id" element={<PostDetailPage />} />
+    <GlobalContext.Provider value={{ posts }} >
+      <BrowserRouter>
+        <Routes>
+          {/* DefaultLayout viene utilizzato come layout di default per le rotte */}
+          <Route element={<DefaultLayout />}>
+            {/* Definizione delle rotte */}
+            <Route path="/" element={<HomePage />} />
+            <Route path="/chisiamo" element={<ChiSiamo />} />
+            <Route path="/posts">
+              <Route index element={<PostListPage />} />
+              <Route path="create" element={<PostCreatePage />} />
+              <Route path=":id" element={<PostDetailPage />} />
+            </Route>
+
           </Route>
 
-        </Route>
 
-
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </GlobalContext.Provider>
   )
 }
 
